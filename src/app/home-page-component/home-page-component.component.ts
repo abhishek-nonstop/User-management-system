bbd24d430ee828678c85d4702e306c4c47e99abd
abhishek-nonstop <abhishek.raut@nonstop.com>
2023-01-03 10:48:01 +0530

    post functionality added

--- src/app/home-page-component/home-page-component.component.ts
+++ src/app/home-page-component/home-page-component.component.ts
@@ -1,5 +1,7 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,OnInit,ChangeDetectorRef,OnChanges,DoCheck } from '@angular/core';
 import { HomePageService } from './homepage.service';
+import { Router } from '@angular/router';
+import { timeInterval } from 'rxjs';
 
 @Component({
   // selector: 'app-home-page-component',
@@ -9,16 +11,85 @@ import { HomePageService } from './homepage.service';
 
 export class HomePageComponentComponent implements OnInit {
   
-  constructor(private userServise : HomePageService){};
+  constructor(private userServise : HomePageService,private router:Router,private change:ChangeDetectorRef){};
   searchField:string="";
   products:any[]=[];
- 
+  openForm:boolean=false;
+
+  nameField:string="";
+  addressField:string="";
+  emailField:string="";
+  companyField:string="";
+  mobileField:string="";
+
+  userDetail = {
+    address:
+      "",
+    company
+      :
+      "",
+    education
+      : [{institute: 'PICT', pass_out_year: '2'}],
+    email
+      :
+      "",
+    experience
+      :
+      [],
+    gender
+      :
+      "",
+    hobbies
+      : ['Reading', 'Music'],
+    name
+      :
+      "",
+    phone
+      :
+      "",
+    profile_picture
+      :
+      "",
+    skills
+      :
+      ['react','javaScript','python','node']
+  }
+
+  
   handleClick():void{
     console.log("click handled");
   }
   handleChange():void{
     console.log('change detected',this.searchField)
   }
+
+
+  handleFormOpen():void{
+    this.openForm=!this.openForm;
+  }
+  handleCloseBtn(){
+    this.handleFormOpen();
+  }
+  handleSubmit(){
+    if(this.nameField!=="" && this.addressField!=="" && this.emailField!=="" && this.mobileField!=="" &&this.companyField!==""){
+      const updateUserObject=this.userDetail;
+      updateUserObject.name=this.nameField;
+      updateUserObject.address=this.addressField;
+      updateUserObject.email=this.emailField;
+      updateUserObject.phone=this.mobileField;
+      updateUserObject.company=this.companyField
+      
+      this.userServise.createUser(updateUserObject).subscribe((data:any)=>{
+        if(data.id){
+          console.log("data submitted");
+        }
+      })
+    }
+    else{
+      console.log('empty field')
+    }
+  }
+
   private async fetchUser(){
     this.products=await this.userServise.getUser();
   }
@@ -27,7 +98,9 @@ export class HomePageComponentComponent implements OnInit {
     this.fetchUser()
     console.log(this.products,"jajksdbjbakj")
   }
-
+  ngDoCheck(){
+    this.change.detectChanges()
+  }
   
 
 
--- src/app/home-page-component/homepage.service.ts
+++ src/app/home-page-component/homepage.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from "rxjs";
 
 @Injectable({
     providedIn:"root"
 })
 export class HomePageService{
-
+    constructor( private http:HttpClient){}
     
     async getUser(){
         const userResponse=await fetch('https://60d5a2c2943aa60017768b01.mockapi.io/candidate')
@@ -13,4 +15,7 @@ export class HomePageService{
         return data
     }
 
+    createUser(userObject:any){
+       return  this.http.post('https://60d5a2c2943aa60017768b01.mockapi.io/candidate',userObject)
+    }
 }
